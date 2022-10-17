5bd2c292375d6770ec7be81f9765bd902b53bfbc
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2022-10-17 12:00:26 +0530

    Update vcf_filter.py

--- vcf_filter.py
+++ vcf_filter.py
@@ -1,15 +1,16 @@
 import os
 # print(os.listdir())
 
-def vcf_filter(infile,outfile,filton):
+def vcf_filter(infile,outfile,filton,start):
     """Filters a VCF file with the filtering option in the Info column alone
         infile : input vcf file
         outfile : output vcf file
-        filton : term to filter in the INFO field"""
+        filton : term to filter in the INFO field
+        start : starting for the header"""
     vcf_in = open(infile,'r',encoding='utf-8',newline='\n')
     vcf_out = open(outfile, 'a',encoding='utf-8',newline='\n')
-    vcf_data = vcf_in.readlines()[34:]
-    vcf_header = vcf_in.readlines()[:34]
+    vcf_data = vcf_in.readlines()[start:] #
+    vcf_header = vcf_in.readlines()[:start]
     for i in vcf_header:
         vcf_out.write(i)
     for j in vcf_data:
@@ -19,24 +20,3 @@ def vcf_filter(infile,outfile,filton):
 
     vcf_in.close()
     vcf_out.close()
-
-
-# vcf_in = open("merged_test.vcf",'r',encoding='utf-8',newline='\n')
-# vcf_out = open("merged_filtered.vcf",'w',encoding='utf-8',newline='\n')
-
-# vcf_lines = vcf_in.readlines()[34:]
-# # print(vcf_lines[:2])
-# vcf_header = vcf_in.readlines()[:34]
-# for i in vcf_header:
-#     vcf_out.write(str(i)+"\n")
-# for j in vcf_lines:
-#     vcf_ann = j.split('\t')[7].split("|")[1]
-#     # print(vcf_ann)
-#     if vcf_ann != "synonymous_variant":
-#         vcf_out.write(str(j)+"\n")
-
-
-# vcf_in.close()
-# vcf_out.close()
-
-vcf_filter("B12510.vcf","B12510_filt.vcf","synonymous")
