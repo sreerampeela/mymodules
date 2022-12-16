aa9de2a17ebcd1d6aaa38940f7d0fdb065e6f7c4
sreerampeela <59723039+sreerampeela@users.noreply.github.com>
2022-12-16 12:11:01 +0530

    Update get_network.py

--- get_network.py
+++ get_network.py
@@ -18,7 +18,7 @@ def cluster_proteins(prot_seqs_files):
     with open("total_prots.fasta",'a') as fout:
         for rec in all_prots:
             SeqIO.write(rec,fout,"fasta")
-    cmd = f"/mnt/d/cd-hit-v4.8.1-2019-0228/cd-hit -i total_prots.fasta -o spn_clustered_prots.fasta -c 0.9 -T 4 -p 1"
+    cmd = f"/mnt/d/cd-hit-v4.8.1-2019-0228/cd-hit -i total_prots.fasta -o spn_clustered_prots.fasta -c 0.9 -T 4 -p 1" #add full path to CD-HIT
     print("Starting clustering of proteins...")
     os.system(cmd)
 
@@ -35,14 +35,13 @@ def get_string_network(fname):
         string_id = record.id
         print(string_id)
         params = {
-
             "identifiers" : string_id, 
             "species" : 2, 
-            "caller_identity" : "srirambds.murthy@gmail.com", 
-            "required_score" : 700
+            "caller_identity" : "srirambds.murthy@gmail.com", #specify email/app name
+            "required_score" : 700 # specify score
         }
         response = requests.post(request_url, data=params)
         with open("string_interactions.tab",'a') as fout:
             fout.write(response.text)
         print(f"Written protein interactions for {string_id}")
-        os.system("sleep 10")
+        os.system("sleep 10") #timeout 10 for windows systems
